2c59df880158242147c7da43a6a382f7315fe71d
jeremy <jeremy9106@gamil.com>
2020-09-08 00:24:44 +0900

    [admob tutorial]

---
+++ admonTest/admonTest/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  admonTest
+//
+//  Created by Jh Rim on 2020/09/07.
+//  Copyright © 2020 CodingAtNight. All rights reserved.
+//
+
+import UIKit
+import GoogleMobileAds
+class ViewController: UIViewController {
+    var bannerView: GADBannerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bannerView = GADBannerView(adSize: kGADAdSizeBanner)
+        addBannerViewToView(bannerView)
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        
+    }
+    func addBannerViewToView(_ bannerView: GADBannerView) {
+     bannerView.translatesAutoresizingMaskIntoConstraints = false
+     view.addSubview(bannerView)
+     view.addConstraints(
+       [NSLayoutConstraint(item: bannerView,
+                           attribute: .bottom,
+                           relatedBy: .equal,
+                           toItem: bottomLayoutGuide,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 0),
+        NSLayoutConstraint(item: bannerView,
+                           attribute: .centerX,
+                           relatedBy: .equal,
+                           toItem: view,
+                           attribute: .centerX,
+                           multiplier: 1,
+                           constant: 0)
+       ])
+    }
+
+}
+
